daffbb79734d5ba2fa444e24e85b8382b1a02e9d
Dniel K <danielu96@wp.pl>
2023-03-31 23:57:57 +0200

    addItem works

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
 import ToDo from './ToDo'
+import Items from './Items'
+import { nanoid } from 'nanoid'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [items,setItems] = useState([]);
+
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      id:nanoid(),
+    };
+    setItems([...items, newItem])
+  }
 
   return (
     <>
      <h1>Vite-ToDo </h1>
-    <ToDo/>
+    <ToDo addItem={addItem}/>
+    <Items items={items} />
     <div className="App">             
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
