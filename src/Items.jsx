c61725a3ffb7d8a6eae782c9b7e044fdc8713b95
Dniel K <danielu96@wp.pl>
2023-04-04 22:52:24 +0200

    removeItem added

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import SingleItem from './SingleItem'
 
-const Items = ({items}) => {
+const Items = ({items,removeItem}) => {
   return (
+   
     items.map((item)=> {
-      return <SingleItem key={item.id} item={item}/>
+      return <SingleItem key={item.id} item={item} removeItem={removeItem}/>
+     
     })
-    
+   
   )
+  
+  
 }
 
 export default Items
