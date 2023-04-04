88cd44cb156f1e958f34d61b4d62cd6cea650bde
Dniel K <danielu96@wp.pl>
2023-04-04 23:11:15 +0200

    getLocalStorage added and works

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import ToDo from './ToDo'
 import Items from './Items'
 import { nanoid } from 'nanoid'
 import './App.css'
+const getLocalStorage = () => {
+  let items = localStorage.getItem('items');
+  if (items) {
+    return (items = JSON.parse(localStorage.getItem('items')));
+  } else {
+    return [];
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [items,setItems] = useState([]);
+  // const [count, setCount] = useState(0);
+  const [items,setItems] = useState(getLocalStorage());
 
 
   const addItem = (itemName) => {
@@ -27,7 +35,9 @@ function App() {
      setItems([])
      console.log('er')
   }
-
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items));
+  }, [items]);
   return (
     <>
      <h1>Vite-ToDo </h1>
@@ -35,11 +45,11 @@ function App() {
     <Items items={items} removeItem={removeItem} />
     <div className="App">  
     <p>{items.length}</p>
-    <button onClick={()=> clearItems()}>clear</button>           
+    <button onClick={()=> clearItems()}>clear all</button>           
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>      
+        </button>       */}
       </div>
       <p className="read-the-docs">
         will be todo 
