166d338306b544975fd5ef9f84ae2b89d6cc72a0
Dniel K <danielu96@wp.pl>
2023-04-05 11:13:43 +0200

    editItem added

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
-const SingleItem = ({item,removeItem}) => {
+const SingleItem = ({item,removeItem,editItem}) => {
  
   return (
     <section style={{display:'flex', justifyContent:'space-between'}}>
+    <button
+    type='button'
+    className='edit-btn'
+    onClick={() => editItem(id)}
+  >
+    <FaEdit />
+  </button>
     <h2>{item.name}</h2>
     {/* <span>{item.id}</span> */}
      <button onClick={() => removeItem(item.id)}>x</button>
