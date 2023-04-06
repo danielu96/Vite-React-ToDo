e02b5800b1b89138dcbb90c2f1f3eb37bc7b205f
Dniel K <danielu96@wp.pl>
2023-04-06 16:56:01 +0200

    Alert.jsx created

---
+++ src/Alert.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+
+const Alert = ({ type, msg, removeAlert, items }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [items]);
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+};
+
+export default Alert;
