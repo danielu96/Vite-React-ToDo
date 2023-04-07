7421b6ae9d2400c490dc1056f5d1da1bfeaa6fb3
Dniel K <danielu96@wp.pl>
2023-04-07 19:53:27 +0200

    items.map working

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,16 +1,57 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 import SingleItem from './SingleItem'
+import EditForm from'./EditForm'
 
-const Items = ({items,removeItem,editItem,toggleComplete}) => {
+const Items = ({item,items,removeItem,editItem,toggleComplete,addItems}) => {
+  const [isEditing, setIsEditing] = useState(false);
+ 
   return (
+   items.map((item)=>{
+   return <SingleItem key={item.id}item={item} editItem={editItem} removeItem={removeItem}
+   addItems={addItems}toggleComplete={toggleComplete}/>
+   }
+   ))
+  
+ 
+  //  {
+  //   item.isEditing ? (
+    
+  //   <EditForm key={item.id}item={item} editItem={editItem} removeItem={removeItem}
+  //   addItems={addItems}toggleComplete={toggleComplete}/>
+    
+  //  ):
+  //  (
+  
+  //   <SingleItem key={item.id}item={item} editItem={editItem} removeItem={removeItem}
+  //     addItems={addItems}toggleComplete={toggleComplete}/>
    
-    items.map((item)=> {
-      // const { id, title } = item;
-      return <SingleItem key={item.id} item={item} removeItem={removeItem} editItem={editItem} toggleComplete={toggleComplete}/>
-     
-    })
+  //  )}
+
+// {isEditing ? (
+//   items.map( (item) => {
+  
+//     return   <SingleItem key={item.id} item={item} removeItem={removeItem} editItem={editItem} 
+//     toggleComplete={toggleComplete}addItem={addItems}/>
+//    })
+//     ) 
+
+// }
+
+
+  //    items.map((item)=> {
+  //     isEditing ? (
+  //       <EditForm item={item} editItem={editItem}/>
+  //     ): (
+  //    <SingleItem key={item.id} item={item} removeItem={removeItem} editItem={editItem} 
+  // toggleComplete={toggleComplete}addItem={addItems}/>
+  //     )
+  //   }
+  
+  //   ) 
+  //       )
    
-  )
+  
   
   
 }
