afc2d967d972c6d56bb5ccc50aa41fe9d65927e3
Dniel K <danielu96@wp.pl>
2023-04-08 19:04:21 +0200

    <p> on <article> changed

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -10,8 +10,8 @@ const SingleItem = ({item,removeItem,editItem,toggleComplete,renameItem,handleSu
     <section style={ {display:'flex', justifyContent:'space-between'}}>
       <button style={{width:'4.1rem'}}>
       <div type='checkbox' onClick={() => toggleComplete(item.id)}
-      // className={`${item.completed ? 'completed': ''}
-      // `}     
+      className={`${item.completed ? 'completed': ''}
+      `}     
       >
       {!item.completed ?  'undone' : 'done'}  </div>
       </button>
@@ -21,7 +21,7 @@ const SingleItem = ({item,removeItem,editItem,toggleComplete,renameItem,handleSu
     className={`${item.completed ? 'completed': ''}
     `}
     > {item.name}</p> */}
-<p >{
+<article >{
 !editMode ? (
 <div onClick={()=> setEditMode(true)}>{item.name}</div>  
 ):(
@@ -34,16 +34,11 @@ const SingleItem = ({item,removeItem,editItem,toggleComplete,renameItem,handleSu
     onChange={(event) => setNewItemName(event.target.value)}
     value={newItemName}
     />
-    <button  className='button' onClick={renameItem} >  
-            
-            update </button>
+  <button className='button' onClick={handleSubmit} >  update </button>
   </form>
   
-  </div>
-
-
-)
-}</p>
+  </div>)
+}</article>
     
     <button
     type='button'
@@ -54,7 +49,10 @@ const SingleItem = ({item,removeItem,editItem,toggleComplete,renameItem,handleSu
   
     <FaEdit />
   </button>
-     <button onClick={() => removeItem(item.id)}>x</button>
+     <button 
+      onClick={() => removeItem(item.id)}
+    //  onClick={()=> setEditMode(true)}
+     >x</button>
     
      
     </section>
