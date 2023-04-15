04d6772017146bd72356c800f8666668d1fa3fee
Dniel K <danielu96@wp.pl>
2023-04-15 17:15:09 +0200

    alert works

--- src/Alert.jsx
+++ src/Alert.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react';
 
-const Alert = ({ type, msg, removeAlert, items }) => {
+const Alert = ({ type, msg, removeAlert, tasks }) => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       removeAlert();
     }, 3000);
     return () => clearTimeout(timeout);
-  }, [items]);
-  return <p className={`alert alert-${type}`}>{msg}</p>;
+  }, [tasks]);
+  return <p   className=  {`alert alert-${type}`}  
+  >   
+    {msg}
+  </p>;
 };
-
 export default Alert;
+
+
+
